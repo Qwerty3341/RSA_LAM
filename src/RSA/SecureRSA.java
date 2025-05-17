e9d0d2266e98ffa02ac4d31f6eeeaf0565c501e7
Luciano <lucaaron00@gmail.com>
2025-05-16 18:00:57 -0600

    RSA implementado

---
+++ src/RSA/SecureRSA.java
@@ -0,0 +1,35 @@
+package RSA;
+
+import javax.crypto.Cipher;
+import java.security.*;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+public class SecureRSA {
+    private static final int KEY_SIZE = 2048;
+    private PrivateKey privateKey;
+    private PublicKey publicKey;
+
+    public SecureRSA() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(KEY_SIZE);
+        KeyPair pair = keyGen.generateKeyPair();
+        this.privateKey = pair.getPrivate();
+        this.publicKey = pair.getPublic();
+    }
+
+    public String encrypt(String message) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedBytes = cipher.doFinal(message.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public String decrypt(String encrypted) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+        return new String(decryptedBytes);
+    }
+}
